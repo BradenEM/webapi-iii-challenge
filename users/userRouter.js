de56c290b58ff7a13bdb43f607917be819d8842c
Braden Mason <bradenemason@gmail.com>
2019-07-31 20:25:39 -0500

    endpoints for users all working

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Users = require("./userDb");
+const Posts = require("../posts/postDb");
 const router = express.Router();
 
 router.post("/", async (req, res) => {
@@ -12,7 +13,15 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.post("/:id/posts", async (req, res) => {});
+router.post("/:id/posts", async (req, res) => {
+  try {
+    const postAddition = { user_id: req.params.id, text: req.body.text };
+    const post = await Posts.insert(postAddition);
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 router.get("/", async (req, res) => {
   try {
@@ -45,9 +54,27 @@ router.get("/:id/posts", async (req, res) => {
   }
 });
 
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    user = await Users.remove(id);
 
-router.put("/:id", async (req, res) => {});
+    res.status(200).json("user has been escorted out");
+  } catch (error) {
+    res.status(500).jsone(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await Users.update(id, req.body);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 //custom middleware
 
